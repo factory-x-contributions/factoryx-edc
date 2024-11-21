19d863f0e0bdbadd4f5a301c14194bc4fe88720d
Anooj Ramakrishnan <anooj.ramakrishnan@t-systems.com>
2024-11-21 12:14:17 +0530

    cleared copyrights

--- edc-dataplane/edc-dataplane-base/build.gradle.kts
+++ edc-dataplane/edc-dataplane-base/build.gradle.kts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2024 T-Systems International GmbH
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
