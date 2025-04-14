0b151f559dbd217567ea4b2fd273603a54cc43c8
Anooj Ramakrishnan <anooj.ramakrishnan@t-systems.com>
2025-04-14 13:25:09 +0530

    corrected checkstyle violations

--- edc-extensions/fx-validator/src/test/java/org/factoryx/edc/validators/didselector/AssetTypeValidatorTest.java
+++ edc-extensions/fx-validator/src/test/java/org/factoryx/edc/validators/didselector/AssetTypeValidatorTest.java
@@ -1,22 +1,35 @@
+/********************************************************************************
+ * Copyright (c) 2025 T-Systems International GmbH
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 package org.factoryx.edc.validators.didselector;
 
-import jakarta.json.JsonArrayBuilder;
 import org.assertj.core.api.Assertions;
 import org.eclipse.edc.validator.jsonobject.JsonObjectValidator;
 import org.eclipse.edc.validator.spi.ValidationFailure;
 import org.eclipse.edc.validator.spi.Violation;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
-
 import static jakarta.json.Json.createArrayBuilder;
 import static jakarta.json.Json.createObjectBuilder;
 import static org.assertj.core.api.InstanceOfAssertFactories.list;
-import static org.eclipse.edc.connector.controlplane.policy.spi.PolicyDefinition.EDC_POLICY_DEFINITION_TYPE;
-import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.*;
+import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.ID;
 import static org.eclipse.edc.junit.assertions.AbstractResultAssert.assertThat;
-import static org.eclipse.edc.spi.constants.CoreConstants.EDC_NAMESPACE;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AssetTypeValidatorTest {
 
