1be2e8292898ab3d2281a8b292ec86475665067a
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2026-01-28 10:44:47 +0100

    fix: add consumer dplane

--- resources/local-fx-mvd/additional_config/pg_init/pg_init.sql
+++ resources/local-fx-mvd/additional_config/pg_init/pg_init.sql
@@ -2,6 +2,7 @@
     CREATE DATABASE cons_ih_db;
     CREATE DATABASE prov_ih_db;
     CREATE DATABASE cons_cpl;
+    CREATE DATABASE cons_dpl;
     CREATE DATABASE prov_cpl;
     CREATE DATABASE prov_dpl;
 
